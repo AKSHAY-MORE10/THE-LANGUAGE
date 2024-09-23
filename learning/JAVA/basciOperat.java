e0a96a9d2ae61fbf4ed65a661d601c371a2ed68d
AKHSAY-MORE10 <akshaybapumore10@gmail.com>
2024-09-23 20:26:32 +0530

    array practice

--- learning/JAVA/basciOperat.java
+++ learning/JAVA/basciOperat.java
@@ -8,11 +8,26 @@ public class basciOperat {
         // transpose of array 
     int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-    for (int i = 0; i < arr.length; i++) {
-        for (int j = 0; j < arr[i].length; j++) {
-            System.out.print(arr[j][i] + " ");
-        }
-        System.out.println();
+
+    //! transpose of arrays
+    // for (int i = 0; i < arr.length; i++) {
+    //     for (int j = 0; j < arr[i].length; j++) {
+    //         System.out.print(arr[j][i] + " ");
+    //     }
+    //     System.out.println();
+    // }
+
+
+   int sum = 0;
+    for (int i = 0; i<arr.length; i++){
+        for (int j = 0; j<arr[i].length; j++){
+            sum += arr[i][j];
     }
+    System.out.println(sum);
+    }
+
+
+
+
     }
 }
