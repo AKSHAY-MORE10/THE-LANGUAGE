5a6411db2c84e2adcbec2bd733b06caf915ac411
AKHSAY-MORE10 <akshaybapumore10@gmail.com>
2024-09-26 23:12:43 +0530

    logical Q ptc

--- learning/JAVSCRIPT/jslivenote/logicpc.js
+++ learning/JAVSCRIPT/jslivenote/logicpc.js
@@ -3,14 +3,14 @@
 // **
 //*
 
-// let n = 4
-// for (let i = 0; i<n; i++){
-//     for (let j = 0; j < n-i; j++){
-//         console.log("x")
-//     }
-//     console.log("\n");
-    
-// }
+let n = 4
+for (let i = 0; i<n; i++){
+    for (let j = 0; j < n-i; j++){
+        console.log("x")
+    }
+    console.log("\n");
+   
+}
 
 // let n = 10;
 // let fact = 1;
